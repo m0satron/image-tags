3ea4b627c74e7c6ae4784c5c4961c2537c59c815
Mosa M.Sebdani <msebdani@gmail.com>
2020-07-15 13:09:42 +0200

    add basic components add sass

---
+++ frontend/src/components/Photo/Photo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="photo">
+    <img class="photo__image" :src="imgSrc" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Photo",
+  props: {
+    coordinates: Object,
+    tag: String,
+    imgSrc: String
+  }
+};
+</script>
+
+<style lang="scss" src="./photo.scss">
---
+++ frontend/src/components/Uploader/Uploader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="file" @change="onFileSelected" />
+    <Photo :imgSrc="selectedFile" />
+  </div>
+</template>
+
+<script>
+import Photo from "../Photo/Photo";
+
+export default {
+  name: "Uploader",
+  components: {
+    Photo
+  },
+  data() {
+    return {
+      selectedFile: null
+    };
+  },
+  methods: {
+    onFileSelected(event) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (e) => {
+        this.selectedFile = e.target.result;
+      }
+    }
+  }
+};
+</script>
